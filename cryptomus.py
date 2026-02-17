ed35923adcfa43ef430502d07e883cdba98c1a1f
nashatmohamed990-art <nashatmohamed990@gmail.com>
2026-02-17 14:43:07 +0400

    Create cryptomus.py

---
+++ cryptomus.py
@@ -0,0 +1,6 @@
+[InternetShortcut]
+URL=https://github.com/snoups/3xui-shop/blob/main/app/bot/payment_gateways/cryptomus.py
+IDList=
+HotKey=0
+IconFile=C:\Users\Nashaat\AppData\Local\Mozilla\Firefox\Profiles\s2tswmlc.default-release\shortcutCache\r0VgMpvPbV1s1s1fqM0wgWBHk5q33wSivXbYSRLwUlQ=.ico
+IconIndex=0
