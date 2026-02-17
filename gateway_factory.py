fca37f21fdb2c0464189a7c3f8ea1e4e97417bd7
nashatmohamed990-art <nashatmohamed990@gmail.com>
2026-02-17 14:43:50 +0400

    Create gateway_factory.py

---
+++ gateway_factory.py
@@ -0,0 +1,6 @@
+[InternetShortcut]
+URL=https://github.com/snoups/3xui-shop/blob/main/app/bot/payment_gateways/gateway_factory.py
+IDList=
+HotKey=0
+IconFile=C:\Users\Nashaat\AppData\Local\Mozilla\Firefox\Profiles\s2tswmlc.default-release\shortcutCache\hTsLzbZbTlyXyc9CQI1ESY_gHz7gPGCw9DI_w3viMOw=.ico
+IconIndex=0
