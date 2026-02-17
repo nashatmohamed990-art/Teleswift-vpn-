4beef64080a743bdc6d70e543d717706453cb56e
nashatmohamed990-art <nashatmohamed990@gmail.com>
2026-02-17 14:41:40 +0400

    Create _init_.py

---
+++ _init_.py
@@ -0,0 +1,6 @@
+[InternetShortcut]
+URL=https://github.com/snoups/3xui-shop/blob/main/app/bot/payment_gateways/__init__.py
+IDList=
+HotKey=0
+IconFile=C:\Users\Nashaat\AppData\Local\Mozilla\Firefox\Profiles\s2tswmlc.default-release\shortcutCache\xGqJ1hPyNfU3BPj6bzD53w+StjO7EE8Ffs6VqsJVerE=.ico
+IconIndex=0
