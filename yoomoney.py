35a3dd094225dca7c3b3de845803652788847f02
nashatmohamed990-art <nashatmohamed990@gmail.com>
2026-02-17 14:45:39 +0400

    Create yoomoney.py

---
+++ yoomoney.py
@@ -0,0 +1,6 @@
+[InternetShortcut]
+URL=https://github.com/snoups/3xui-shop/blob/main/app/bot/payment_gateways/yoomoney.py
+IDList=
+HotKey=0
+IconFile=C:\Users\Nashaat\AppData\Local\Mozilla\Firefox\Profiles\s2tswmlc.default-release\shortcutCache\ZzoqAdxC4C1xIwyA6v+VTB3U1yB57uhVmp6yIlfDvHY=.ico
+IconIndex=0
