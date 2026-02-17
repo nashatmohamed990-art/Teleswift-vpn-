4c9e9d28dc7af03269439647c1582db282e39eb5
nashatmohamed990-art <nashatmohamed990@gmail.com>
2026-02-17 14:44:19 +0400

    Create heleket.py

---
+++ heleket.py
@@ -0,0 +1,6 @@
+[InternetShortcut]
+URL=https://github.com/snoups/3xui-shop/blob/main/app/bot/payment_gateways/heleket.py
+IDList=
+HotKey=0
+IconFile=C:\Users\Nashaat\AppData\Local\Mozilla\Firefox\Profiles\s2tswmlc.default-release\shortcutCache\AyQz3_9AU4FPlGwI2LOVjAVAq90T7v5KiRl3h9mhZnQ=.ico
+IconIndex=0
