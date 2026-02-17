38d98b26865b1dcd2285c3e30b3d302c25fa9bdc
nashatmohamed990-art <nashatmohamed990@gmail.com>
2026-02-17 14:45:12 +0400

    Create yookassa.py

---
+++ yookassa.py
@@ -0,0 +1,6 @@
+[InternetShortcut]
+URL=https://github.com/snoups/3xui-shop/blob/main/app/bot/payment_gateways/yookassa.py
+IDList=
+HotKey=0
+IconFile=C:\Users\Nashaat\AppData\Local\Mozilla\Firefox\Profiles\s2tswmlc.default-release\shortcutCache\hf0cHD5lzNJB1MuRiIbIl3cRiYd4GSNZ48_yTcaePvI=.ico
+IconIndex=0
