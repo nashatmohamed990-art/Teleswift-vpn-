91dbd705ff078d2797e9e3a8e96ade7c14dc94f7
nashatmohamed990-art <nashatmohamed990@gmail.com>
2026-02-17 14:42:26 +0400

    Create _gateway.py

---
+++ _gateway.py
@@ -0,0 +1,6 @@
+[InternetShortcut]
+URL=https://github.com/snoups/3xui-shop/blob/main/app/bot/payment_gateways/_gateway.py
+IDList=
+HotKey=0
+IconFile=C:\Users\Nashaat\AppData\Local\Mozilla\Firefox\Profiles\s2tswmlc.default-release\shortcutCache\E_BejUSr3sLTHSFSix90I9Bfg58QHtni6NpfxmRcwng=.ico
+IconIndex=0
