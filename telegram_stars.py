3c9b2360e88269e9166036b1105420a6e21384dc
nashatmohamed990-art <nashatmohamed990@gmail.com>
2026-02-17 14:44:47 +0400

    Create telegram_stars.py

---
+++ telegram_stars.py
@@ -0,0 +1,6 @@
+[InternetShortcut]
+URL=https://github.com/snoups/3xui-shop/blob/main/app/bot/payment_gateways/telegram_stars.py
+IDList=
+HotKey=0
+IconFile=C:\Users\Nashaat\AppData\Local\Mozilla\Firefox\Profiles\s2tswmlc.default-release\shortcutCache\L3NbIy464v5aJXF6llhudXBjU9dacGn5Fimp0kdfMZQ=.ico
+IconIndex=0
